ed9c9a234e6d784cde9a32f3394756449ff1055d
Nikita Nigmatullin <nnigmat@gmail.com>
2021-12-02 09:52:00 +0300

    feat: day 2, deep

---
+++ day_02_deep/task1.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf-8');
+
+const finalPosition = input.split('\n').reduce(({ x, y }, value) => {
+	const [direction, amountString] = value.split(' ');
+	const amount = Number(amountString);
+
+	if (direction === 'forward') {
+		return { x: x + amount, y };
+	} else if (direction === 'up') {
+		return { x, y: y - amount };
+	} else {
+		return { x, y: y + amount };
+	}
+}, { x: 0, y: 0 });
+
+const { x, y } = finalPosition;
+const result = x * y;
+
+console.log('Result', result);
